1d600f6d7972963958424f0f7786848486d16098
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-07-01 12:04:36 +0100

    Added notes on incoming JMS document queue vs JMS pending 'request send' queue

--- src/main/java/uk/nhs/ciao/transport/spine/SpineTransportRoutes.java
+++ src/main/java/uk/nhs/ciao/transport/spine/SpineTransportRoutes.java
@@ -32,6 +32,19 @@ public class SpineTransportRoutes extends CIPRoutes {
 		final CIAOConfig config = CamelApplication.getConfig(getContext());
 		// TODO: Complete routes
 		
+		/*
+		 * Documents to send are stored on a JMS queue
+		 * Prior to sending the document needs to be converted into
+		 * a multi-part request with associated ebXml, hl7, and ITK parts
+		 * This multi-part message needs to persist until an ebXml ack is received
+		 *  -> a transaction and blocking thread can be used to handle retrys and failover
+		 * 
+		 * It might be be best to take the original document off the original queue, transform it
+		 * into the outgoing message (generating associated tracking / message IDs), and then add
+		 * it to a 'sending' queue - this would ensure that an identical outgoing message it sent
+		 * during retries (including metadata such as creation time etc)
+		 */
+		
 //		try {
 //			
 //		} catch (CIAOConfigurationException e) {
