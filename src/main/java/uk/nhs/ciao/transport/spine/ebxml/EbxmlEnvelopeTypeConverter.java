e800d502cc568dcbe2e6683bb7b89bfb34b126fc
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-07-28 11:11:34 +0100

    Added Camel type converter support for xml -> EbxmlEnvelope Added unit tests for EbxmlEnvelopeTypeConverter

---
+++ src/main/java/uk/nhs/ciao/transport/spine/ebxml/EbxmlEnvelopeTypeConverter.java
@@ -0,0 +1,106 @@
+package uk.nhs.ciao.transport.spine.ebxml;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.camel.Converter;
+import org.apache.camel.Exchange;
+import org.apache.camel.FallbackConverter;
+import org.apache.camel.TypeConverter;
+import org.apache.camel.spi.TypeConverterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Throwables;
+import com.google.common.io.Closeables;
+
+/**
+ * Camel type converters to convert <strong>to</strong> EbxmlEnvelope
+ * <p>
+ * To convert an EbxmlEnvelope to XML, a route of
+ * <code>to("freemarker:uk/nhs/ciao/transport/spine/ebxml/ebxmlEnvelope.ftl")</code> can be used.
+ * <p>
+ * The converters are automatically registered via Camel's type converter META-INF/services file:
+ * <code>/META-INF/services/org/apache/camel/TypeConverter</code>
+ */
+@Converter
+public class EbxmlEnvelopeTypeConverter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EbxmlEnvelopeTypeConverter.class);
+	
+	/**
+	 * Holds a single parser instance per thread (the parsers are not thread-safe)
+	 */
+	private static final ThreadLocal<EbxmlEnvelopeParser> PARSER = new ThreadLocal<EbxmlEnvelopeParser>() {
+		@Override
+		protected EbxmlEnvelopeParser initialValue() {
+			try {
+				return new EbxmlEnvelopeParser();
+			} catch (Exception e) {
+				LOGGER.error("Unable to create EbxmlEnvelopeParser", e);
+				throw Throwables.propagate(e);
+			}
+		}
+	};
+	
+	/**
+	 * Converts the specified input stream to a EbxmlEnvelope
+	 * <p>
+	 * The InputStream is not closed by this method.
+	 */
+	@Converter
+	public static EbxmlEnvelope fromInputStream(final InputStream in) throws IOException {
+		LOGGER.debug("fromInputStream()");
+		
+		final EbxmlEnvelopeParser parser = PARSER.get();
+		return parser.parse(in);
+	}
+	
+	/**
+	 * Camel fallback converter to convert a value to EbxmlEnvelope to a specified type either directly or via InputStream as an intermediate.
+	 * <p>
+	 * The type converter registry is used to convert the value to InputStream.
+	 */
+	@FallbackConverter
+	public static <T> T convertToEbxmlEnvelope(final Class<T> type, final Exchange exchange, final Object value, final TypeConverterRegistry registry) throws IOException {
+		if (!EbxmlEnvelope.class.equals(type)) {
+			// Only handle EbxmlEnvelope conversions
+			return null;
+		} else if (value instanceof EbxmlEnvelope) {
+			// No conversion required
+			return type.cast(value);
+		}
+		
+		LOGGER.debug("convertToEbxmlEnvelope via (InputStream) from: {}", value.getClass());
+		
+		// Convert via InputStream
+		final InputStream in = castOrConvert(InputStream.class, exchange, value, registry);
+		try {
+			return in == null ? null : type.cast(fromInputStream(in));
+		} finally {
+			// close the stream if it is an intermediate
+			// does the stream always need to be closed? will camel close the stream when in == value?
+			if (in != value) {
+				Closeables.closeQuietly(in);
+			}
+		}
+	}
+	
+	/**
+	 * Helper method to coerce the specified value into the required type.
+	 * <p>
+	 * Value is cast if it is already of the correct kind, otherwise a camel type converter is tried. Null is
+	 * returned if the value cannot be coerced.
+	 */
+	private static <T> T castOrConvert(final Class<T> toType, final Exchange exchange, final Object value, final TypeConverterRegistry registry) {
+		final T result;
+		
+		if (toType.isInstance(value)) {
+			result = toType.cast(value);
+		} else {
+			final TypeConverter typeConverter = registry.lookup(toType, value.getClass());
+			result = typeConverter == null ? null : typeConverter.convertTo(toType, exchange, value);
+		}
+		
+		return result;
+	}
+}
---
+++ src/test/java/uk/nhs/ciao/transport/spine/ebxml/EbxmlEnvelopeTypeConverterTest.java
@@ -0,0 +1,116 @@
+package uk.nhs.ciao.transport.spine.ebxml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultProducerTemplate;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closeables;
+
+/**
+ * Tests for {@link EbxmlEnvelopeTypeConverter}
+ */
+public class EbxmlEnvelopeTypeConverterTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EbxmlEnvelopeTypeConverterTest.class);
+	
+	private CamelContext context;
+	private ProducerTemplate producerTemplate;
+	
+	@Before
+	public void setup() throws Exception {
+		context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("direct:deserialize")
+					.streamCaching()
+					.log(LoggingLevel.INFO, LOGGER, "${body}")
+					.convertBodyTo(EbxmlEnvelope.class);
+			}
+		});
+		
+		context.start();
+		producerTemplate = new DefaultProducerTemplate(context);
+		producerTemplate.start();
+	}
+	
+	@After
+	public void teardown() {
+		if (producerTemplate != null) {
+			try {
+				producerTemplate.stop();
+			} catch (Exception e) {
+				LOGGER.warn("Unable to stop producerTemplate", e);
+			}
+		}
+		
+		if (context != null) {
+			try {
+				context.stop();
+			} catch (Exception e) {
+				LOGGER.warn("Unable to stop context", e);
+			}
+		}
+	}
+	
+	/**
+	 * Tests that an input stream can be converted to an envelope (the standard
+	 * conversion method is tested)
+	 */
+	@Test
+	public void testConvertFromInputStream() throws IOException {
+		final InputStream in = getClass().getResourceAsStream("./test-manifest.xml");
+		try {
+			Assert.assertNotNull(deserialize(in));
+		} finally {
+			Closeables.closeQuietly(in);
+		}
+	}
+	
+	/**
+	 * Tests that a string (i.e. something other than input stream) can be converted to an envelope (the fallback / intermediate
+	 * conversion method is tested)
+	 */
+	@Test
+	public void testConvertFromString() throws IOException {
+		final InputStream in = getClass().getResourceAsStream("./test-ack.xml");
+		Reader reader = null;
+		try {
+			reader = new InputStreamReader(in);
+			final String body = CharStreams.toString(reader);
+			Assert.assertNotNull(deserialize(body));
+		} finally {
+			Closeables.closeQuietly(in);
+		}
+	}
+	
+	/**
+	 * Converts the body to an {@link EbxmlEnvelope} by sending into through
+	 * the configured camel route
+	 */
+	private EbxmlEnvelope deserialize(final Object body) {
+		final Exchange exchange = new DefaultExchange(context);
+		exchange.setPattern(ExchangePattern.InOut);
+		exchange.getIn().setBody(body);
+		
+		producerTemplate.send("direct:deserialize", exchange);
+		return exchange.getOut().getBody(EbxmlEnvelope.class);
+	}
+}
