209a5a090caadfdb4d2c343d348db2e569780bd0
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-07-30 14:54:28 +0100

    Extracted variables in multipart route into bean properties Added additional unit test for invalid multipart body

--- src/main/java/uk/nhs/ciao/transport/spine/route/MultipartMessageReceiverRoute.java
+++ src/main/java/uk/nhs/ciao/transport/spine/route/MultipartMessageReceiverRoute.java
@@ -7,6 +7,7 @@ import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.processor.idempotent.MemoryIdempotentRepository;
+import org.apache.camel.spi.IdempotentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,13 +23,55 @@ import uk.nhs.ciao.transport.spine.multipart.MultipartBody;
  */
 public class MultipartMessageReceiverRoute extends RouteBuilder {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MultipartMessageReceiverRoute.class);
+	private static final String PAYLOAD_PROPERTY = "multipart-payload";
 	
 	// TODO: Make in/out route URLs configurable
+	
+	/**
+	 * URI where incoming multipart messages are received from
+	 * <p>
+	 * input only
+	 */
+	private final String multipartReceiverUri = "direct:multipart-receiver";
+	
+	/**
+	 * URI of internal route to publish outgoing payloads and to create the
+	 * associated async ebxml responses
+	 * <p>
+	 * input and output (internal route)
+	 */
+	private final String payloadPublisherUri = "seda:multipart-payload-publisher";
+	
+	/**
+	 * URI where outgoing payload messages are sent to
+	 * <p>
+	 * output only
+	 */
+	private final String payloadDestinationUri = "mock:multipart-payloads";
+	
+	/**
+	 * URI of internal route to send outgoing ebxml responses
+	 * <p>
+	 * input and output (internal route)
+	 */
+	private final String ebxmlResponseSenderUri = "seda:ebxml-response-sender";	
+	
+	/**
+	 * URI where outgoing ebxml response messages are sent to
+	 * <p>
+	 * output only
+	 */
+	private final String ebxmlResponseDestinationUri = "mock:ebxml-responses";
+	
+	
+	// TODO: Make IdempotentRepository configurable
+	private final IdempotentRepository<?> idempotentRepository = new MemoryIdempotentRepository();
+	
 	@Override
 	public void configure() throws Exception {		
 		configureMultipartReceiver();
 		configurePayloadPublisher();
-		configureEbxmlAckSender();
+		configureEbxmlResponseSender();
 	}
 	
 	/**
@@ -38,12 +81,12 @@ public class MultipartMessageReceiverRoute extends RouteBuilder {
 	 * The payload is extracted and sent for publishing via a separate route
 	 */
 	private void configureMultipartReceiver() {
-		from("direct:multipart-receiver")
+		from(multipartReceiverUri)
 			// Start publishing the payload after the initial HTTP response has been sent
 			.onCompletion().onCompleteOnly()
 				.setExchangePattern(ExchangePattern.InOnly)
-				.setBody(property("multipart-payload"))
-				.to("seda:multipart-payload-publisher")
+				.setBody(property(PAYLOAD_PROPERTY))
+				.to(payloadPublisherUri)
 			.end()
 			
 			.convertBodyTo(MultipartBody.class)
@@ -51,7 +94,7 @@ public class MultipartMessageReceiverRoute extends RouteBuilder {
 			.process(new EbxmlManifestVerifier())
 	
 			// Store the payload in a property so it can be published after the main response is sent
-			.setProperty("multipart-payload").spel("#{body.getParts().get(2).getBody()}")
+			.setProperty(PAYLOAD_PROPERTY).spel("#{body.parts[2].body}")
 			
 			// HTTP sync response
 			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant("200"))
@@ -67,29 +110,29 @@ public class MultipartMessageReceiverRoute extends RouteBuilder {
 	 * determined by the type / content of the payload.
 	 */
 	private void configurePayloadPublisher() {
-		from("seda:multipart-payload-publisher")
+		from(payloadPublisherUri)
 			// On failure - send ebxml delivery failure notification
 			.onCompletion().onFailureOnly()
 				.setBody().property(MANIFEST_PROPERTY)
 				.setBody().spel("#{body.generateDeliveryFailureNotification('Unable to deliver payload')}")
-				.to("seda:ebxml-ack-sender")
+				.to(ebxmlResponseSenderUri)
 			.end()
 	
-			// Publish ITK message for processing - but only if not successfully processed already
+			// Publish payload message for processing - but only if not successfully processed already
 			.idempotentConsumer(simple("${property." + MANIFEST_PROPERTY + ".messageData.messageId}"),
-					new MemoryIdempotentRepository())
+					idempotentRepository)
 			.eager(false)
 			.removeOnFailure(true)
 			.skipDuplicate(false)
 				// only publish if not handled already
 				.filter(property(Exchange.DUPLICATE_MESSAGE).isNull())
-					.to("mock:multipart-payload-receiver")
+					.to(payloadDestinationUri)
 				.end()
 	
 				// always send ebxml ack (i.e. if previously acked or if publishing was successful)
 				.setBody().property(MANIFEST_PROPERTY)
 				.setBody(simple("${body.generateAcknowledgment()}"))
-				.to("seda:ebxml-ack-sender")
+				.to(ebxmlResponseSenderUri)
 			.end()
 		.end();
 	}
@@ -98,9 +141,9 @@ public class MultipartMessageReceiverRoute extends RouteBuilder {
 	 * Route to send outgoing ebxml acknowledgement and delivery fault async responses
 	 */
 	// TODO: does outgoing ack response need retry logic / error hander?
-	private void configureEbxmlAckSender() {
-		from("seda:ebxml-ack-sender")
+	private void configureEbxmlResponseSender() {
+		from(ebxmlResponseSenderUri)
 			.convertBodyTo(String.class)
-			.to("mock:multipart-ack-sender");
+			.to(ebxmlResponseDestinationUri);
 	}
 }
--- src/test/java/uk/nhs/ciao/transport/spine/route/MultipartMessageReceiverRouteTest.java
+++ src/test/java/uk/nhs/ciao/transport/spine/route/MultipartMessageReceiverRouteTest.java
@@ -3,6 +3,7 @@ package uk.nhs.ciao.transport.spine.route;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.InvalidPayloadException;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -28,8 +29,8 @@ public class MultipartMessageReceiverRouteTest {
 	private CamelContext context;
 	private ProducerTemplate producerTemplate;
 	
-	private MockEndpoint payloadReceiverEndpoint;
-	private MockEndpoint ackSenderEndpoint;
+	private MockEndpoint payloadReceiver;
+	private MockEndpoint ebxmlResponseReceiver;
 	
 	@Before
 	public void setup() throws Exception {
@@ -41,8 +42,8 @@ public class MultipartMessageReceiverRouteTest {
 		context.start();
 		producerTemplate.start();
 		
-		payloadReceiverEndpoint = MockEndpoint.resolve(context, "mock:multipart-payload-receiver");
-		ackSenderEndpoint = MockEndpoint.resolve(context, "mock:multipart-ack-sender");
+		payloadReceiver = MockEndpoint.resolve(context, "mock:multipart-payloads");
+		ebxmlResponseReceiver = MockEndpoint.resolve(context, "mock:ebxml-responses");
 	}
 	
 	@After
@@ -73,18 +74,16 @@ public class MultipartMessageReceiverRouteTest {
 		// Sync response should not be a fault
 		Assert.assertNull("SOAPFault", syncResponse);
 		
-		// Payload message should be extracted and published for processing
-		payloadReceiverEndpoint.expectedMessageCount(1);
-		payloadReceiverEndpoint.expectedBodyReceived().constant(getPayloadBody(body));
+		expectSinglePayload(body);
 		
 		// async ack should be sent (after payload message is published)
-		ackSenderEndpoint.expectedMessageCount(1);
+		ebxmlResponseReceiver.expectedMessageCount(1);
 		
-		ackSenderEndpoint.assertIsSatisfied(1000);
-		payloadReceiverEndpoint.assertIsSatisfied();
+		ebxmlResponseReceiver.assertIsSatisfied(1000);
+		payloadReceiver.assertIsSatisfied();
 		
 		// Check that the response is an ack
-		final EbxmlEnvelope ack = ackSenderEndpoint.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+		final EbxmlEnvelope ack = getFirstEbxmlResponse();
 		Assert.assertTrue("Acknowledgment", ack.isAcknowledgment());
 	}
 	
@@ -98,38 +97,37 @@ public class MultipartMessageReceiverRouteTest {
 		Assert.assertNull("SOAPFault", syncResponse);
 		
 		// Payload message should be extracted and published for processing
-		payloadReceiverEndpoint.expectedMessageCount(1);
-		payloadReceiverEndpoint.expectedBodyReceived().constant(getPayloadBody(body));
+		expectSinglePayload(body);
 		
 		// async ack should be sent (after payload message is published)
-		ackSenderEndpoint.expectedMessageCount(1);
+		ebxmlResponseReceiver.expectedMessageCount(1);
 		
-		ackSenderEndpoint.assertIsSatisfied(1000);
-		payloadReceiverEndpoint.assertIsSatisfied();
+		ebxmlResponseReceiver.assertIsSatisfied(1000);
+		payloadReceiver.assertIsSatisfied();
 		
 		// Check that the response is an ack
-		EbxmlEnvelope ack = ackSenderEndpoint.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+		EbxmlEnvelope ack = getFirstEbxmlResponse();
 		Assert.assertTrue("Acknowledgment", ack.isAcknowledgment());
 		
 		// Check duplicate detection by resending message
-		ackSenderEndpoint.reset();
-		payloadReceiverEndpoint.reset();
+		ebxmlResponseReceiver.reset();
+		payloadReceiver.reset();
 		syncResponse = sendMultipartMessage(body);
 		
 		// Sync response should not be a fault
 		Assert.assertNull("SOAPFault", syncResponse);
 		
 		// Payload message should not be published again
-		payloadReceiverEndpoint.expectedMessageCount(0);
+		payloadReceiver.expectedMessageCount(0);
 		
 		// However an async ack should be sent
-		ackSenderEndpoint.expectedMessageCount(1);
+		ebxmlResponseReceiver.expectedMessageCount(1);
 		
-		ackSenderEndpoint.assertIsSatisfied(1000);
-		payloadReceiverEndpoint.assertIsSatisfied();
+		ebxmlResponseReceiver.assertIsSatisfied(1000);
+		payloadReceiver.assertIsSatisfied();
 		
 		// Check that the response is an ack
-		ack = ackSenderEndpoint.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+		ack = getFirstEbxmlResponse();
 		Assert.assertTrue("Acknowledgment", ack.isAcknowledgment());
 	}
 	
@@ -148,11 +146,21 @@ public class MultipartMessageReceiverRouteTest {
 		Assert.assertTrue("Error severity", syncResponse.getError().isError());
 	}
 	
+	@Test(expected=Exception.class)
+	public void testHttpErrorIsSentWhenMultipartBodyIsInvalid() throws Exception {
+		// Send a corrupted request
+		final Exchange exchange = new DefaultExchange(context);
+		exchange.getIn().setBody("this is not a valid multipart-body");
+		
+		// an HTTP error is expected
+		sendMultipartMessage(exchange);
+	}
+	
 	@Test
 	public void testDeliveryFailureIsSentWhenPayloadPublishingFailsWithAnException() throws Exception {
 		final MultipartBody body = createExampleRequest();
 		
-		payloadReceiverEndpoint.whenAnyExchangeReceived(new Processor() {
+		payloadReceiver.whenAnyExchangeReceived(new Processor() {
 			@Override
 			public void process(final Exchange exchange) throws Exception {
 				throw new Exception("Simulating message delivery failure");
@@ -165,12 +173,12 @@ public class MultipartMessageReceiverRouteTest {
 		Assert.assertNull("SOAPFault", syncResponse);
 		
 		// async nack should be sent (after payload message publishing is attempted)
-		ackSenderEndpoint.expectedMessageCount(1);
+		ebxmlResponseReceiver.expectedMessageCount(1);
 		
-		ackSenderEndpoint.assertIsSatisfied(1000);
+		ebxmlResponseReceiver.assertIsSatisfied(1000);
 		
 		// Check that the response was a nack
-		final EbxmlEnvelope nack = ackSenderEndpoint.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+		final EbxmlEnvelope nack = getFirstEbxmlResponse();
 		Assert.assertTrue("DeliveryFailure", nack.isDeliveryFailure());
 		Assert.assertTrue("Warning severity", nack.getError().isWarning());
 	}
@@ -179,7 +187,7 @@ public class MultipartMessageReceiverRouteTest {
 	public void testDeliveryFailureIsSentWhenPayloadPublishingFailsWithAFaultMessage() throws Exception {
 		final MultipartBody body = createExampleRequest();
 		
-		payloadReceiverEndpoint.whenAnyExchangeReceived(new Processor() {
+		payloadReceiver.whenAnyExchangeReceived(new Processor() {
 			@Override
 			public void process(final Exchange exchange) throws Exception {
 				exchange.getOut().setBody("Simulating message delivery failure");
@@ -193,12 +201,12 @@ public class MultipartMessageReceiverRouteTest {
 		Assert.assertNull("SOAPFault", syncResponse);
 		
 		// async nack should be sent (after payload message publishing is attempted)
-		ackSenderEndpoint.expectedMessageCount(1);
+		ebxmlResponseReceiver.expectedMessageCount(1);
 		
-		ackSenderEndpoint.assertIsSatisfied(1000);
+		ebxmlResponseReceiver.assertIsSatisfied(1000);
 		
 		// Check that the response was a nack
-		final EbxmlEnvelope nack = ackSenderEndpoint.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+		final EbxmlEnvelope nack = getFirstEbxmlResponse();
 		Assert.assertTrue("DeliveryFailure", nack.isDeliveryFailure());
 		Assert.assertTrue("Warning severity", nack.getError().isWarning());
 	}
@@ -227,6 +235,11 @@ public class MultipartMessageReceiverRouteTest {
 		return body;
 	}
 	
+	private void expectSinglePayload(final MultipartBody body) throws Exception {
+		payloadReceiver.expectedMessageCount(1);
+		payloadReceiver.expectedBodyReceived().constant(getPayloadBody(body));
+	}
+	
 	private String getPayloadBody(final MultipartBody body) throws Exception {
 		return body.getParts().get(2).getMandatoryBody(String.class);
 	}
@@ -235,15 +248,24 @@ public class MultipartMessageReceiverRouteTest {
 		return context.getTypeConverter().convertTo(String.class, envelope);
 	}
 	
+	private EbxmlEnvelope getFirstEbxmlResponse() throws InvalidPayloadException {
+		return ebxmlResponseReceiver.getExchanges().get(0).getIn().getMandatoryBody(EbxmlEnvelope.class);
+	}
+	
 	private EbxmlEnvelope sendMultipartMessage(final MultipartBody body) throws Exception {
 		final Exchange exchange = new DefaultExchange(context);
-		exchange.setPattern(ExchangePattern.InOut);
 		exchange.getIn().setBody(body, String.class); // convert the body
 
 		final ContentType contentType = new ContentType("multipart", "related");
 		contentType.setBoundary(body.getBoundary());
 		contentType.setStart(body.getParts().get(0).getRawContentId());
 		exchange.getIn().setHeader(Exchange.CONTENT_TYPE, contentType.toString());
+		
+		return sendMultipartMessage(exchange);
+	}
+	
+	private EbxmlEnvelope sendMultipartMessage(final Exchange exchange) throws Exception {
+		exchange.setPattern(ExchangePattern.InOut);
 		
 		producerTemplate.send("direct:multipart-receiver", exchange);
 		if (exchange.getException() != null) {
