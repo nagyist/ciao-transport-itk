8608072d520c7b14365b4a80a336dc45a5ac6d06
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-09-01 15:39:33 +0100

    Collapsed ForwardExpressSenderRoutes into MultipartMessageSenderRoute Removed orphaned classes

--- src/main/java/uk/nhs/ciao/transport/spine/route/MultipartMessageSenderRoute.java
+++ src/main/java/uk/nhs/ciao/transport/spine/route/MultipartMessageSenderRoute.java
@@ -1,16 +1,27 @@
 package uk.nhs.ciao.transport.spine.route;
 
-import static uk.nhs.ciao.transport.spine.forwardexpress.ForwardExpressSenderRoutes.*;
+import java.util.Collections;
+import java.util.Set;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.processor.idempotent.MemoryIdempotentRepository;
 import org.apache.camel.spring.spi.TransactionErrorHandlerBuilder;
+import org.apache.camel.util.toolbox.AggregationStrategies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.SettableFuture;
+
 import uk.nhs.ciao.transport.spine.forwardexpress.EbxmlAcknowledgementProcessor;
+import uk.nhs.ciao.transport.spine.forwardexpress.ForwardExpressMessageExchange;
 
 /**
- * Roue to send outgoing multipart messages over spine and wait for async acknowledgement
+ * Route to send outgoing multipart messages over spine and wait for async acknowledgement
  * <ul>
  * <li>Sends a multi-part trunk request message over the spine
  * <li>Blocks until an async ebXml ack is received off a configured JMS topic or a timeout occurs
@@ -46,7 +57,9 @@ public class MultipartMessageSenderRoute extends BaseRouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		configureMultipartMessageSender();
-		configureForwardExpressHandler();
+		configureForwardExpressSender();
+		configureForwardAckReceiver();
+		configureForwardExpressMessageAggregator();
 	}
 	
 	/**
@@ -70,10 +83,6 @@ public class MultipartMessageSenderRoute extends BaseRouteBuilder {
 			)
 			.transacted("PROPAGATION_NOT_SUPPORTED")
 			
-			// Store a copy of the outgoing request into the documents in-progress folder
-//			.setHeader(Exchange.FILE_NAME, simple("${header." + IN_PROGRESS_FOLDER + "}/trunk-request"))
-//			.to("file://.") // full location is determined by Exchange.FILE_NAME header
-			
 //			.doTry()
 				.to(getForwardExpressHandlerUrl())
 				.process(new EbxmlAcknowledgementProcessor())
@@ -82,6 +91,9 @@ public class MultipartMessageSenderRoute extends BaseRouteBuilder {
 //				.process(new HttpErrorHandler())
 		.end();
 	}
+
+	private final Set<String> inprogressIds = Collections.newSetFromMap(Maps.<String, Boolean>newConcurrentMap());
+	private final int aggregatorTimeout = 30000;
 	
 	/**
 	 * Route to send an HTTP request/response to spine and wait
@@ -93,16 +105,76 @@ public class MultipartMessageSenderRoute extends BaseRouteBuilder {
 	 * <li>or a timeout occurs while waiting for the ack
 	 * <li>or the ack is received
 	 */
-	private void configureForwardExpressHandler() throws Exception {
-		final String requestRouteId = getInternalRoutePrefix();
-		final String ackRouteId = getInternalRoutePrefix() + "-ack";
-		final String aggregateRouteId = getInternalRoutePrefix() + "-aggregator";
+	private void configureForwardExpressSender() throws Exception {
+		from(getForwardExpressHandlerUrl())
+			.routeId(getInternalRoutePrefix())
+			.doTry()
+				.setHeader(ForwardExpressMessageExchange.MESSAGE_TYPE, constant(ForwardExpressMessageExchange.REQUEST_MESSAGE))
+				.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
 		
-		forwardExpressSender(getContext(),
-			from(getForwardExpressHandlerUrl()).routeId(requestRouteId))
-			.to(multipartMessageDestinationUri)
-			.waitForResponse(
-				forwardExpressAckReceiver(ackRouteId, ebxmlAckReceiverUri, "JMSMessageID", "JMSCorrelationID"),
-				forwardExpressMessageAggregator(aggregateRouteId, getForwardExpressAggregatorUrl(), 30000));
+				// TODO: Work out how to maintain the original request body - but also examine the out response from the HTTP request-response call
+				.multicast(AggregationStrategies.useOriginal())
+					.bean(inprogressIds, "add(${header.CamelCorrelationId})")
+					
+					// TODO: Is a pipeline + checking of out message required here?
+					.to(ExchangePattern.InOut, multipartMessageDestinationUri)
+				.end()
+				.setProperty(ForwardExpressMessageExchange.ACK_FUTURE, method(SettableFuture.class, "create"))
+				.to(getForwardExpressAggregatorUrl())
+				.process(new ForwardExpressMessageExchange.WaitForAck(aggregatorTimeout + 1000)) // timeout is slightly higher than the corresponding value in the aggregate
+				.validate().simple("${body.isComplete()}")
+				.transform().simple("${body.getAckBody()}")
+				// If left as the final clause, transform appears to cause problems by
+				// for the next processor in the chain by nulling the exchange on one of the messages
+				// This *appears* to be a bug in how TransformProcessor uses ExchangeHelper.replaceMessage
+				// when making a message copy - the out message is copied but the exchange is nulled on the non-replaced message
+				// An addition processor (which does not need to use getIn().getExchange()) seems to resolve the problem
+				.log(LoggingLevel.DEBUG, LOGGER, "Extracted acknowledgment for ${header.CamelCorrelationId}")
+			.endDoTry()
+			.doFinally()
+				.process(new Processor() {
+					@Override
+					public void process(final Exchange exchange) throws Exception {
+						// Mark the ID as no longer in-progress
+						inprogressIds.remove(exchange.getIn().getHeader(Exchange.CORRELATION_ID));
+					}
+				})
+			.end()
+		.end();
+	}
+	
+	private void configureForwardAckReceiver() throws Exception {
+		final String messageIdHeader = "JMSMessageID";
+		final String correlationIdHeader = "JMSCorrelationID";
+		
+		from(ebxmlAckReceiverUri)
+			.routeId(getInternalRoutePrefix() + "-ack")
+			// multiple threads may be running - only process each incoming ack once
+			.idempotentConsumer(header(messageIdHeader), new MemoryIdempotentRepository())
+			
+			// only handle IDs currently active in this process
+			.filter(method(inprogressIds, "contains(${header." + correlationIdHeader + "})"))
+			.setHeader(Exchange.CORRELATION_ID, header(correlationIdHeader))
+			.setHeader(ForwardExpressMessageExchange.MESSAGE_TYPE, constant(ForwardExpressMessageExchange.ACK_MESSAGE))
+			.log("Incoming ebxml ack for ${header.CamelCorrelationId}")
+			.to(getForwardExpressAggregatorUrl())
+		.end();
+	}
+	
+	/**
+	 * Configures a direct route which aggregates incoming request messages with associated asynchronous ebXML acks
+	 */
+	private void configureForwardExpressMessageAggregator() throws Exception {
+		// Correlate original requests with incoming acks
+		// acks may be received with no corresponding open request (i.e.
+		// the request originated in another process)
+		from(getForwardExpressAggregatorUrl())
+			.routeId(getInternalRoutePrefix() + "-aggregator")
+			.aggregate(header(Exchange.CORRELATION_ID), new ForwardExpressMessageExchange.AggregationStrategy())
+				.completionPredicate(method(ForwardExpressMessageExchange.class, "isComplete(${body})"))
+				.completionTimeout(aggregatorTimeout)
+			.log("Completed forward-express request-response aggregate: ${header.CamelCorrelationId}")
+			.bean(ForwardExpressMessageExchange.class, "notifyCompletion(${body})")
+		.end();
 	}
 }
