e85347bb7503fc8089bdf80b301aa1f94091e230
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-10-15 16:11:08 +0100

    Updated the receiver route to manage data and control file move/delete (where applicable) Updated the route to publish the payload Added the original control file as a header on the payload message

--- ciao-transport-dts/src/main/java/uk/nhs/ciao/transport/dts/route/DTSMessageReceiverRoute.java
+++ ciao-transport-dts/src/main/java/uk/nhs/ciao/transport/dts/route/DTSMessageReceiverRoute.java
@@ -2,20 +2,32 @@ package uk.nhs.ciao.transport.dts.route;
 
 import static uk.nhs.ciao.logging.CiaoCamelLogMessage.camelLogMsg;
 
+import java.io.File;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.regex.Pattern;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.util.FileUtil;
+import org.apache.camel.util.toolbox.AggregationStrategies;
 
 import com.google.common.base.Strings;
 
 import uk.nhs.ciao.camel.BaseRouteBuilder;
 import uk.nhs.ciao.camel.URIBuilder;
+import uk.nhs.ciao.dts.ControlFile;
 import uk.nhs.ciao.logging.CiaoCamelLogger;
 import uk.nhs.ciao.transport.dts.processor.DTSDataFilePoller;
 
 public class DTSMessageReceiverRoute extends BaseRouteBuilder {
 	private static final CiaoCamelLogger LOGGER = CiaoCamelLogger.getLogger(DTSMessageReceiverRoute.class);
+	
+	/**
+	 * Maintains the control file as a header in the payload message
+	 * <p>
+	 * This is required to join up response messages with the correct worflowId
+	 */
+	public static final String HEADER_CONTROL_FILE = "dtsControlFile";
 	
 	private String dtsMessageReceiverUri;
 	private String payloadDestinationUri;
@@ -24,7 +36,7 @@ public class DTSMessageReceiverRoute extends BaseRouteBuilder {
 	private String errorFolder;
 	
 	// optional properties
-	private String dtsFilePrefix = "";
+	private String dtsFilePrefix = ""; // TODO: Check if this is valid for the receiver / IN folder
 	private long dataFilePollingInterval = 200;
 	private int dataFileMaxAttempts = 100; // == 20 seconds
 	
@@ -121,24 +133,86 @@ public class DTSMessageReceiverRoute extends BaseRouteBuilder {
 			.set("inProgressRepository", "#" + inProgressRepositoryId)
 			.set("readLock", "idempotent");
 		
-		// TODO: Manually delete control and data files after processing
 		// Unknown files will be left in the folder (other application may be scanning for them)
-		// Error files will be moved to the error folder
+		// Error control and data files will be moved to the error folder
+		// Successfully processed control and data files are deleted as part of the route
 		uri.set("delete", false)
 			.set("moveFailed", errorFolder);
 		
 		from(uri.toString())
+			.onException(Exception.class)
+				.process(new DataFileMover())
+			.end()
+		
 			.process(LOGGER.info(camelLogMsg("Received incoming DTS control file")
 					.fileName(header(Exchange.FILE_NAME))))
 			.setHeader("controlFileName").header(Exchange.FILE_NAME)
+			.convertBodyTo(ControlFile.class)
 			
-			// TODO: Only wait for a data file if the control file is type == Data
+			// Only handle control files for Data messages
+			.choice()
+				.when(simple("${getMessageType} != Data"))
+					.process(LOGGER.info(camelLogMsg("Received DTS Report control file - stopping processing of file")
+							.fileName(header(Exchange.FILE_NAME))))
+					.stop()
+				.endChoice()
+			.end()
+			
+			// TODO: Does the workflowId and/or address need checking???
 			
 			// Wait for the associated data file
 			.setHeader(DTSDataFilePoller.HEADER_FOLDER_NAME).header(Exchange.FILE_PARENT)
 			.setHeader(DTSDataFilePoller.HEADER_FILE_NAME, regexReplaceAll(
 					simple("${header.CamelFileName}"), "(..*)\\.ctl", "$1.dat"))
 			.process(new DTSDataFilePoller(executorService, dataFilePollingInterval, dataFileMaxAttempts))
+			
+			// Publish the payload (using multicast/pipeline to maintin original message)
+			.multicast(AggregationStrategies.useOriginal())
+				.pipeline()
+					.setProperty(HEADER_CONTROL_FILE).body(String.class)
+					.setBody().header(DTSDataFilePoller.HEADER_FILE)
+					.convertBodyTo(byte[].class)
+					
+					// Store the control file - it may be required for future DTS exchanges (especially the workflowId)
+					.removeHeaders("*")
+					.setHeader(HEADER_CONTROL_FILE).property(HEADER_CONTROL_FILE)
+					.removeProperty(HEADER_CONTROL_FILE)
+					
+					.to(payloadDestinationUri)
+				.end()
+			.end()
+			
+			// If successful delete the pair of files
+			.process(new ControlFileAndDataFileDeleter())
 		.end();
+	}
+	
+	/**
+	 * If successful, the control and data file are deleted
+	 */
+	private class ControlFileAndDataFileDeleter implements Processor {
+		@Override
+		public void process(final Exchange exchange) throws Exception {
+			final File dataFile = exchange.getIn().getHeader(DTSDataFilePoller.HEADER_FILE, File.class);
+			final File controlFile = new File(dataFile.getParentFile(),
+					exchange.getIn().getHeader(Exchange.FILE_NAME, String.class));
+			
+			FileUtil.deleteFile(dataFile);
+			FileUtil.deleteFile(controlFile);
+		}
+	}
+	
+	/**
+	 * If successful, the data file is moved to the error folder
+	 */
+	private class DataFileMover implements Processor {
+		@Override
+		public void process(final Exchange exchange) throws Exception {
+			final File dataFile = exchange.getIn().getHeader(DTSDataFilePoller.HEADER_FILE, File.class);
+			if (dataFile != null && dataFile.isFile()) {
+				final File folder = dataFile.getParentFile();
+				FileUtil.renameFile(dataFile, new File(folder, errorFolder), true);
+			}
+		}
 	}
 }
